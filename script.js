9dbf3bda5cfd961c7196d5aab0ab46031c3e3276
Liana Meyer <liana_p@icloud.com>
2020-05-18 18:41:16 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let leaveEnq = document.querySelector('.button1');
+let enq = document.querySelector('.enq');
+let enq1 = document.querySelector('.enq1');
+let enq2 = document.querySelector('.enq2');
+let enqFields = document.querySelector('.enq-fields');
+
+console.log(leaveEnq);
+console.log(enq);
+console.log(enq1);
+console.log(enq2);
+
+leaveEnq.onclick = function () {
+  enq1.classList.toggle('enq-1');
+  enq2.classList.toggle('enq-2');
+  enqFields.classList.toggle('enq-fields-show');
+};
